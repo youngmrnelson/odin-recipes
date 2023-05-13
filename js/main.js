b4142bac9bfbbbeed39b220d819685452da08ab6
Daniel Nelson <codemrnelson@gmail.com>
2023-05-13 18:12:08 -0400

    Create template page for dashboard and recipe object

--- js/main.js
+++ js/main.js
@@ -24,3 +24,17 @@ navIcons.forEach(icon => {
         icon.classList.remove('fa-fade');
     })
 })
+
+// Recipes Object
+function Recipe(recipeName, calories) {
+    this.recipeName = recipeName;
+    this.calories = calories;
+    this.info = function() {
+        console.log(recipeName);
+    }
+}
+
+const recipe = new Recipe('Salad', 113);
+recipe.info();
+
+console.log(Object.getPrototypeOf(recipe) === Recipe.prototype);
