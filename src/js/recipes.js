cb099128e8b7e7e9f3e89bb6abe1e46be5d65661
Daniel Nelson <codemrnelson@gmail.com>
2023-05-22 14:27:05 -0400

    Create Fetch API call to a recipes API

--- src/js/recipes.js
+++ src/js/recipes.js
@@ -1,7 +1,7 @@
 // Recipes Constructor
 export default class Recipe {
-  constructor(recipeName, calories, ingredients) {
-    this.recipeName = recipeName;
+  constructor(title, calories, ingredients) {
+    this.title = title;
     this.calories = calories;
     this.ingredients = ingredients;
   }
